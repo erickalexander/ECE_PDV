a578dbfde8a97db40dc8054b41d5f8de30e061d7
Erick Camacho <erickcamacho@dhcp-10-5-23-121.wireless.rochester.edu>
2017-04-19 19:34:07 -0400

    have added some controller code

--- erick_pdv.py
+++ erick_pdv.py
@@ -1,1 +1,22 @@
-#new
+#Gamepad controller 710
+#Make sure to install kernel interpreter evdev
+        ##  Check for gamepad information   ##
+
+from evdev import InputDevice, categorize, ecodes
+gamepad = InputDevice('/dev/input/event0')
+print gamepad
+
+
+        ##  Recognizes when button pressed  ##
+
+from evdev import InputDevice, categorize, ecodes
+gamepad = InputDevice('/dev/input/event0')
+for event in gamepad.readloop():
+    print event
+
+
+                    #sample output#
+
+## event at 1432
+
+##
